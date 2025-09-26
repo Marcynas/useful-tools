df1245d7f7390a64bbc609b05e91edf07dac475e
Martynas Tvaska <73237433+Marcynas@users.noreply.github.com>
2025-09-26 18:35:44 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/usefull-tools/",
+  base: "/useful-tools/",
     plugins: [vue(),
     tailwindcss()
   ],
